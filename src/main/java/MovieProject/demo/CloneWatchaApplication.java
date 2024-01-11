af909718b538f4f234ac61bfc2b4f4d38cfb7523
romay <i_romay@naver.com>
2024-01-11 17:58:50 +0900

    Initial commit

---
+++ src/main/java/MovieProject/demo/CloneWatchaApplication.java
@@ -0,0 +1,13 @@
+package MovieProject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneWatchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneWatchaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MovieProject/demo/CloneWatchaApplicationTests.java
@@ -0,0 +1,13 @@
+package MovieProject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneWatchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
