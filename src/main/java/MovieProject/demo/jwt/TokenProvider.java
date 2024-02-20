5ae1de35792748daf4b9198b5deabdfa71bda976
romay <i_romay@naver.com>
2024-02-20 18:44:56 +0900

    240220
    gradle jwt 관련 의존성, 스프링 시큐리티 의존성 추가
    
    Security 관련 설정파일 추가

---
+++ src/main/java/MovieProject/demo/jwt/TokenProvider.java
@@ -0,0 +1,20 @@
+package MovieProject.demo.jwt;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class TokenProvider {
+    private final Key key;
+
+    public TokenProvider(@Value("${jwt.secret}") String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+}
